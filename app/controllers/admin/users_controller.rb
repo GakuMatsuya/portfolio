2129dbf1721ff9c80358ab7fc34cead29d58d651
GakuMatsuya <gaku.1237@icloud.com>
2021-02-21 08:38:05 +0000

    [追加] edit/updateアクション,strong paramater

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,8 +5,19 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(admin_user_params)
+    redirect_to admin_users_path
   end
+
+  private
+
+  def admin_user_params
+    params.require(:user).permit(:is_active)
+  end
+
 end
