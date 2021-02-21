195045f8ffa1d662af31cc2c0f3df5bf4d7cba5f
GakuMatsuya <gaku.1237@icloud.com>
2021-02-21 08:08:42 +0000

    [更新] is_active enumの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  #ユーザー情報が有効な時true,退会済みユーザのときはfalseが
+         
   enum is_active:{
-    effectiveness:        true,
-    withdrawn:            false
+    effectiveness:        true,    #有効なユーザー
+    withdrawn:            false    #退会済みユーザー
    }
 end
