75ab47209bc96d8f7088521027d0c36953517c00
GakuMatsuya <gaku.1237@icloud.com>
2021-02-21 08:17:51 +0000

    [削除] showアクション

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,9 +3,6 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  def show
-  end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :items, except:[:destroy]
-    resources :users, except:[:new, :create, :destroy]
+    resources :users, except:[:new, :create, :show, :destroy]
   end
 
 end
