570016e43344a814def9ac6eaf26baaaf41bffe6
GakuMatsuya <gaku.1237@icloud.com>
2021-02-21 01:35:11 +0000

    [追加] admin/itemのルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  devise_for :admins, controllers: {
+  
+  devise_for :admin, controllers: {
     sessions: "admin/sessions/sessions"
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    resources :items, except:[:destroy]
+  end
+
 end
