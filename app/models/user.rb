37051d54cd7921af0d317e43a17db9ce69ad87c3
GakuMatsuya <gaku.1237@icloud.com>
2021-02-21 07:10:27 +0000

    [追加] 退会フラグのenum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  #ユーザー情報が有効な時true,退会済みユーザのときはfalseが
+  enum is_active:{
+    effectiveness:        true,
+    withdrawn:            false
+   }
 end
