7be8ab0e962ab44f9662d62c14b062d7719072d3
GakuMatsuya <gaku.1237@icloud.com>
2021-02-22 12:46:33 +0000

    [追加] アクションの追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,27 @@
 class Public::UsersController < ApplicationController
+  
+  def show 
+  end
+
+  def edit 
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def following 
+  end
+
+  def followers
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
 end
