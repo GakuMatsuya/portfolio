7a3dcb50c110c0e5a4769c5f942c0dedf00519b6
GakuMatsuya <gaku.1237@icloud.com>
2021-02-22 13:20:04 +0000

    [追加] public/usersコントローラ,devise userのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   
-  devise_for :user, controllers: {
+  #urlを設定
+  devise_for :users, path: '', controllers: {
     sessions: "public/sessions/sessions"
   }
-
+  
   devise_for :admin, controllers: {
     sessions: "admin/sessions/sessions"
   }
   
+  #urlにadminをつけることで、ユーザー側と区別
   namespace :admin do
     resources :items, except:[:destroy]
     resources :users, except:[:new, :create, :show, :destroy]
+  end
+  
+  #ユーザー側のurlにはファイル階層の記述はしない
+  scope module: :public do
+    resources :users, except:[:create, :new, :destroy]
   end
 
 end
