8ab4b756b85d60a621c99ba6f652966dbb8434cf
GakuMatsuya <gaku.1237@icloud.com>
2021-02-23 08:13:35 +0000

    [Add] relationshipモデルのアソシエーション

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,12 @@
 class Relationship < ApplicationRecord
+  
+  belongs_to :user
+
+  #userクラスにbelongs_toするよう、補足設定
+  belongs_to :follow, class_name: "User"
+  
+  #両方のidが揃っていないと保存されないように設定
+  validates :user_id, presence: true
+  validates :follow_id, presence: true
+
 end
