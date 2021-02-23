99000962c3837716d2746f9d590709c0b166e654
GakuMatsuya <gaku.1237@icloud.com>
2021-02-23 03:18:32 +0000

    [Add] attachmentメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  #メソッドを持たせる
+  attachment :profile_image
          
   enum is_active:{
     effectiveness:        true,    #有効なユーザー
