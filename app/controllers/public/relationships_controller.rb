f31c0472c980f16f0537980fde37066ad8912d46
GakuMatsuya <gaku.1237@icloud.com>
2021-02-24 14:16:08 +0000

    [Add] フォロー、アンフォローの処理

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,31 @@
 class Public::RelationshipsController < ApplicationController
+  before_action :set_user
+  
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      flash[:success] = "フォローしました"
+      redirect_to user_path(@user)
+    else
+      flash[:danger] = "フォローに失敗しました"
+      redirect_to user_path(@user)
+    end
+  end
+  
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:success] = "フォローを解除しました"
+      redirect_to user_path(@user)
+    else
+      flash[:danger] = "フォロー解除に失敗しました"
+      redirect_to user_path(@user)
+    end
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:relationship][:follow_id])
+  end
+
 end
