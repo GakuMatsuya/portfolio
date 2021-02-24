3a33476869e1a0cb1d03a3579c9e48635f22d8d8
GakuMatsuya <gaku.1237@icloud.com>
2021-02-24 13:45:47 +0000

    [Update] コードのレイアウト

--- db/migrate/20210223074128_create_relationships.rb
+++ db/migrate/20210223074128_create_relationships.rb
@@ -1,9 +1,11 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
+  
   def change
     create_table :relationships do |t|
-    #user_id,follow_idをカラムとして持たせる。中間テーブルなので、データ型はreferences
-    #また、外部キーとして設定するため、オプションで指定
+  
+    #中間テーブルなので、データ型はreferences。外部キーとして設定するため、オプションで指定
       t.references :user, foreign_key: true
+    
     #follow_idの参照先として、usersテーブルを指定
       t.references :follow, foreign_key: { to_table: :users }
 
