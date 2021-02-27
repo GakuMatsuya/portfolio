5caf2c51777bfc9d5748056b4cf1582f84f752c5
GakuMatsuya <gaku.1237@icloud.com>
2021-02-27 05:49:30 +0000

    [Add] raty.jsの導入

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,11 @@
 class Review < ApplicationRecord
   
   belongs_to :user
+  
+  #レートを1以上5以下設定
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
 
 end
