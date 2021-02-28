fa4c7c3701fdeb91180b0b7657c0a89d5fd0c324
GakuMatsuya <gaku.1237@icloud.com>
2021-02-28 11:40:59 +0000

    [Add] レビューの平均点を取得

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,6 +6,13 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @reviews = @item.reviews
+    
+    #レビューがあれば平均を、なければ0を
+    if @item.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @item.reviews.average(:rate)
+    end
   end
 
 end
