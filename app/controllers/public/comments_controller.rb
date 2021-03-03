d5df275672defd80c782e21997e90a953eae5da9
GakuMatsuya <gaku.1237@icloud.com>
2021-03-03 14:27:46 +0000

    [Add] コメント削除機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -11,10 +11,13 @@ class Public::CommentsController < Public::ApplicationController
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to item_review_path(comment.review.item_id, comment.review_id)
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text)
   end
