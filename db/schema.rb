ee0f4940d627af79da5dec6e38e2ee364677c33c
GakuMatsuya <gaku.1237@icloud.com>
2021-03-06 01:29:15 +0000

    [Add] likesテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_131343) do
+ActiveRecord::Schema.define(version: 2021_03_06_012834) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_03_01_131343) do
     t.string "name"
     t.string "image_id"
     t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "review_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
