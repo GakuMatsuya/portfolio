b9f7d4e745b68bc5b307d1d4c4bc2fc8b20bae4e
GakuMatsuya <gaku.1237@icloud.com>
2021-03-07 06:21:31 +0000

    [Add] 退会処理のルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  
+
   #urlを設定
   devise_for :users, path: '', controllers: {
     sessions: "public/sessions/sessions",
     registrations: "public/sessions/registrations"
   }
-  
+
   devise_for :admin, controllers: {
     sessions: "admin/sessions/sessions"
   }
-  
+
   get "/" => "public/homes#top"
   post "like/:id" => "public/likes#create", as: "create_like"
   delete "like/:id" => "public/likes#destroy", as: "destroy_like"
-  
+
   #urlにadminをつけることで、ユーザー側と区別
   namespace :admin do
     resources :items, except:[:destroy]
     resources :users, except:[:new, :create, :show, :destroy]
   end
-  
+
   #ユーザー側のurlにはファイル階層の記述はしない
   scope module: :public do
     resources :users, except:[:create, :new, :destroy] do
@@ -28,10 +28,11 @@ Rails.application.routes.draw do
       get :likes, on: :member
       get :timeline, on: :member
       get :unsubscribe, on: :member
+      patch :withdraw, on: :member
     end
     resources :items, only: [:index, :show] do
       resources :reviews, except:[:index] do
-        resources :comments, only: [:create, :destroy] 
+        resources :comments, only: [:create, :destroy]
       end
     end
     resources :relationships, only: [:create, :destroy]
