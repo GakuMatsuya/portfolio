6aa6e3351f51401ee9266c4eb98831c477a4a7cc
GakuMatsuya <gaku.1237@icloud.com>
2021-03-08 13:42:38 +0000

    [Update] 名前の空白を制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
+  validates :name, presence: true
+  
   #すでにフォローしていればtrueを返す
   def following?(other_user)
     self.followings.include?(other_user)
