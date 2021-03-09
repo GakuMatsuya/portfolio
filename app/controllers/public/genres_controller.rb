f9c57d5a5261c622b8072b17dd5fe46a0f84f21c
GakuMatsuya <gaku.1237@icloud.com>
2021-03-09 14:06:26 +0000

    [Update] 商品の平均評価を取得

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -3,6 +3,15 @@ class Public::GenresController < ApplicationController
   def index
     @genre = Genre.find(params[:id])
     @items = Item.where("genre_id = #{@genre.id}")
+    
+    @items.each do |item|
+     #レビューがあれば平均を、なければ0を
+      if item.reviews.blank?
+        @average_review = 0
+      else
+        @average_review = item.reviews.average(:rate).round(1)
+      end
+    end
   end
 
 end
