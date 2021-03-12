fab7a549e20247b7001b7414110079c1040f3f75
GakuMatsuya <gaku.1237@icloud.com>
2021-03-12 23:45:15 +0000

    [Update] 空の商品登録を制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :reviews, dependent: :destroy
   attachment :image
+  
+  validates :genre_id, :name, :image, :introduction, presence: true 
 end
