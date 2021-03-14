bdddb2e0e50b8e31df141009e0c1c494d1463842
GakuMatsuya <gaku.1237@icloud.com>
2021-03-14 01:45:58 +0000

    [Add] searchアクション

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,10 +1,22 @@
 class Public::GenresController < ApplicationController
+  before_action :set_q, only: [:index, :search]
 
   def index
     @genre = Genre.find(params[:id])
-    
+
     #itemテーブルにreviewテーブルを結合
     @items = Item.all.left_joins(:reviews).group(:id).select('items.*, count(reviews.item_id) as count, avg(reviews.rate) as average').where(genre_id: @genre.id)
+  end
+
+  def search
+    @items = @q.result
+  end
+
+  private
+
+  #パラメータを元にデータを検索、@qに格納
+  def set_q
+    @q = Item.all.left_joins(:reviews).group(:id).select("items.*, count(reviews.item_id) as count, avg(reviews.rate) as average").ransack(params[:q])
   end
 
 end
