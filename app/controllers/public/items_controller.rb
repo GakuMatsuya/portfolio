92fc9677d58e2f4f228abf4451407b9613ffcefa
GakuMatsuya <gaku.1237@icloud.com>
2021-03-14 00:22:58 +0000

    [Add] searchアクション

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,5 @@
 class Public::ItemsController < Public::ApplicationController
+  before_action :set_q, only: [:index, :search]
 
   def show
     @item = Item.find(params[:id])
@@ -11,10 +12,21 @@ class Public::ItemsController < Public::ApplicationController
       @average_review = @item.reviews.average(:rate).round(1)
     end
   end
-  
+
   #itemテーブルにreviewテーブルを
   def index
     @items = Item.all.left_joins(:reviews).group(:id).select("items.*, count(reviews.item_id) as count, avg(reviews.rate) as average")
+  end
+
+  def search
+    @items = @q.result
+  end
+
+  private
+
+  #パラメータを元にテーブルからデータを検索
+  def set_q
+    @q = Item.all.left_joins(:reviews).group(:id).select("items.*, count(reviews.item_id) as count, avg(reviews.rate) as average").ransack(params[:q])
   end
 
 end
