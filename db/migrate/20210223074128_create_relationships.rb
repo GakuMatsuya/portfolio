99d9f05d0d54e582a0ad436dc5b7319b2295436c
GakuMatsuya <gaku.1237@icloud.com>
2021-03-15 13:19:17 +0000

    [Update] relationshipsモデルのカラム名

--- db/migrate/20210223074128_create_relationships.rb
+++ db/migrate/20210223074128_create_relationships.rb
@@ -3,16 +3,14 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
   
-    #中間テーブルなので、データ型はreferences。外部キーとして設定するため、オプションで指定
-      t.references :user, foreign_key: true
     
-    #follow_idの参照先として、usersテーブルを指定
-      t.references :follow, foreign_key: { to_table: :users }
+      t.integer :follower_id
+      t.integer :following_id
 
       t.timestamps
 
     #重複してフォローされないようにする
-      t.index [:user_id, :follow_id], unique: true
+      t.index [:follower_id, :following_id], unique: true
     end
   end
 end
