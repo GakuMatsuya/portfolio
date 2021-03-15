f9cba9b6a583a0142dd5e85c84947c7f3d953362
GakuMatsuya <gaku.1237@icloud.com>
2021-03-15 13:28:27 +0000

    [Update] create/destroyアクション

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,31 +1,13 @@
 class Public::RelationshipsController < Public::ApplicationController
-  before_action :set_user
-
+  
   def create
-    following = current_user.follow(@user)
-    if following.save
-      flash[:notice] = "フォローしました"
-      redirect_to user_path(@user)
-    else
-      flash[:alert] = "フォローに失敗しました"
-      redirect_to user_path(@user)
-    end
+    @user = User.find(params[:following_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:notice] = "フォローを解除しました"
-      redirect_to user_path(@user)
-    else
-      flash[:alert] = "フォロー解除に失敗しました"
-      redirect_to user_path(@user)
-    end
+    @user = User.find(params[:id])
+    current_user.unfollow(@user)
   end
-
-  private
-  def set_user
-    @user = User.find(params[:relationship][:follow_id])
-  end
-
+  
 end
