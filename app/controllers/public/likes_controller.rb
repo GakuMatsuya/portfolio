8c38f91ed269f4a44a3bea767fe6b25e784bdc0f
GakuMatsuya <gaku.1237@icloud.com>
2021-03-17 11:37:20 +0000

    [Update] @userの定義

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,12 +1,14 @@
 class Public::LikesController < ApplicationController
 
   def create
+    @user = current_user
     @review = Review.find(params[:id])
     like = current_user.likes.build(review_id: params[:id])
     like.save
   end
 
   def destroy
+    @user = current_user
     @review = Review.find(params[:id])
     like = Like.find_by(user_id: current_user.id, review_id: params[:id])
     like.destroy
