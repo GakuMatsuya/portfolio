f33064f631b5f62c573c50e8ed77199e1e68f06f
GakuMatsuya <gaku.1237@icloud.com>
2021-03-17 12:07:20 +0000

    [Update] flashメッセージの設定

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,13 +1,25 @@
 class Public::RelationshipsController < Public::ApplicationController
-  
+
   def create
     @user = User.find(params[:user][:following_id])
-    current_user.follow(@user)
+    if current_user.follow(@user)
+      flash[:notice] = "フォローしました"
+    else
+      flash[:alert] = "フォローに失敗しました"
+    end
+    #リロード後、メッセージが残らないようにする
+    flash.discard
   end
 
   def destroy
     @user = User.find(params[:id])
-    current_user.unfollow(@user)
+    if current_user.unfollow(@user)
+      flash[:notice] = "フォローを解除しました"
+    else
+      flash[:alert] = "フォロー解除に失敗しました"
+    end
+    #リロード後、メッセージが残らないようにする
+    flash.discard
   end
-  
+
 end
