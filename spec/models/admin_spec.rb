dd56a0aa8f7df88bef21e66f9fbe4d4fe19fc5b2
GakuMatsuya <gaku.1237@icloud.com>
2021-03-21 05:52:16 +0000

    [Add] adminモデルのテスト

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Adminモデルのテスト", type: :model do
+  describe "バリデーションのテスト" do
+    it "emailが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+      admin = Admin.new(email: "", encrypted_password: "hogehoge")
+      expect(admin).to be_invalid
+      expect(admin.errors[:email]).to include("を入力してください")
+    end
+    it "encrypted_passwordが空白の場合にバリデーションチェックされるか" do
+      admin = Admin.new(email: "hogehoge@mail.com", encrypted_password: "")
+      expect(admin).to be_invalid
+    end
+  end
+end
