0c55da98a9613bec408e960b7c1aa288aff08e59
GakuMatsuya <gaku.1237@icloud.com>
2021-03-22 13:57:25 +0000

    [Add] ログアウトテスト

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -20,7 +20,7 @@ describe "ユーザーログイン前のテスト" do
         expect(page).to have_field "user[password_confirmation]"
       end
     end
-    
+
     context "新規登録成功のテスト" do
       before do
         fill_in "user[name]", with: Faker::Lorem.characters(number: 10)
@@ -28,7 +28,7 @@ describe "ユーザーログイン前のテスト" do
         fill_in "user[password]", with: "password"
         fill_in "user[password_confirmation]", with: "password"
       end
-      
+
       it "正しく新規登録される" do
         expect { click_button "新規登録" }.to change(User.all, :count).by(1)
       end
@@ -41,33 +41,54 @@ describe "ユーザーログイン前のテスト" do
 
   describe "ユーザーログイン" do
     let(:user) { FactoryBot.create(:user) }
-    
+
     before do
       visit new_user_session_path
     end
-    
+
     context "ログイン成功のテスト" do
       before do
         fill_in "user[email]", with: user.email
         fill_in "user[password]", with: user.password
         click_button "ログイン"
       end
-      
+
       it "ログイン後のリダイレクト先が、ログインしたユーザーの詳細画面になっている" do
         expect(current_path).to eq "/users/" + user.id.to_s
       end
     end
-    
+
     context "ログイン失敗のテスト" do
       before do
         fill_in "user[email]", with: ""
         fill_in "user[password]", with: ""
         click_button "ログイン"
       end
-      
+
       it "ログインに失敗した際、ログイン画面にリダイレクト" do
         expect(current_path).to eq "/sign_in"
       end
     end
   end
-end 
+
+  describe "ユーザーログアウトのテスト" do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      visit new_user_session_path
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: user.password
+      click_button "ログイン"
+
+      #3番目のリンクを取得し、クリック
+      logout_link = find_all("a")[3].native.inner_text
+      click_link logout_link
+    end
+
+    context "ログアウト機能のテスト" do
+      it "ログアウト後のリダイレクト先が、トップ画面になっている" do
+        expect(current_path).to eq "/"
+      end
+    end
+  end
+end
