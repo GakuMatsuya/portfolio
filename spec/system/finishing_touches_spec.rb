5ae0be9e9abf1647e20729b9f080860a8fdeffcc
GakuMatsuya <gaku.1237@icloud.com>
2021-03-24 06:14:46 +0000

    [Add] プロフィール/レビュー編集時のサクセスメッセージテスト

--- spec/system/finishing_touches_spec.rb
+++ spec/system/finishing_touches_spec.rb
@@ -5,6 +5,7 @@ describe "仕上げのテスト" do
   let!(:other_user) { create(:user) }
   let!(:review) { create(:review, user: user) }
   let!(:other_review) { create(:review, user: other_user) }
+  let!(:item) { create(:item) }
 
   describe "サクセスメッセージのテスト" do
     subject { page }
@@ -35,6 +36,28 @@ describe "仕上げのテスト" do
       logout_link = find_all("a")[3].native.inner_text
       click_link logout_link
       is_expected.to have_content "ログアウトしました"
+    end
+    
+    it "ユーザーのプロフィール更新成功時" do
+      visit new_user_session_path
+      visit new_user_session_path
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: user.password
+      click_button "ログイン"
+      visit edit_user_path(user)
+      click_button "編集内容を保存"
+      is_expected.to have_content "変更内容を保存しました"
+    end
+    
+    it "投稿レビューの更新成功時" do
+      visit new_user_session_path
+      visit new_user_session_path
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: user.password
+      click_button "ログイン"
+      visit edit_item_review_path(item, review)
+      click_button "変更する"
+      is_expected.to have_content "変更内容を保存しました"
     end
   end
 end
