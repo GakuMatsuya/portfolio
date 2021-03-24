10d29adbdac50cf9fcffa14daf3333f90fcfd330
GakuMatsuya <gaku.1237@icloud.com>
2021-03-24 08:08:59 +0000

    [Add] 未ログイン時アクセス制限のテスト

--- spec/system/finishing_touches_spec.rb
+++ spec/system/finishing_touches_spec.rb
@@ -6,8 +6,10 @@ describe "仕上げのテスト" do
   let!(:review) { create(:review, user: user) }
   let!(:other_review) { create(:review, user: other_user) }
   let!(:item) { create(:item) }
+  let!(:genre) { create(:genre) }
 
   describe "サクセスメッセージのテスト" do
+    #pageがテスト対象
     subject { page }
 
     it "ユーザー新規登録成功時" do
@@ -80,6 +82,60 @@ describe "仕上げのテスト" do
         click_button "新規登録"
         expect(page).to have_content "6文字以上で入力してください"
       end
+    end
+  end
+  
+  describe "未ログイン時のアクセス制限のテスト: アクセスできず、ログイン画面に遷移" do
+    #current_pathがテスト対象
+    subject { current_path }
+    
+    it "ユーザー詳細画面" do
+      visit user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "ユーザー情報編集画面" do
+      visit edit_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "フォロワー一覧画面" do
+      visit followers_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "フォロー一覧画面" do
+      visit following_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "いいね一覧画面" do
+      visit likes_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "タイムライン画面" do
+      visit timeline_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "退会画面" do
+      visit unsubscribe_user_path(user)
+      is_expected.to eq "/sign_in"
+    end
+    it "投稿詳細画面" do
+      visit item_review_path(item, review)
+      is_expected.to eq "/sign_in"
+    end
+    it "投稿編集画面" do
+      visit edit_item_review_path(item, review)
+      is_expected.to eq "/sign_in"
+    end
+    it "商品一覧画面" do
+      visit items_path
+      is_expected.to eq "/sign_in"
+    end
+    it "ジャンル別商品一覧画面" do
+      visit "genres?id=1"
+      is_expected.to eq "/sign_in"
+    end
+    it "商品詳細画面" do
+      visit item_path(item)
+      is_expected.to eq "/sign_in"
     end
   end
 end
