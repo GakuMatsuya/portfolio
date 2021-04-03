d1eb8f41bf05331f8bbfb2dad089652d371cfd70
GakuMatsuya <gaku.1237@icloud.com>
2021-04-03 12:02:55 +0000

    [update] indexアクション→showアクション

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,11 +1,9 @@
 class Public::GenresController < Public::ApplicationController
   before_action :set_q
-
-  def index
+  
+  def show
     @genre = Genre.find(params[:id])
-
-    #itemテーブルにreviewテーブルを結合
-    @items = Item.all.left_joins(:reviews).group(:id).select('items.*, count(reviews.item_id) as count, avg(reviews.rate) as average').where(genre_id: @genre.id)
+    @items = Item.all.left_joins(:reviews).group(:id).select('items.*, count(reviews.item_id) as count, avg(reviews.rate) as average').where(genre_id: @genre.id).page(params[:page]).per(5)
   end
 
   def search
